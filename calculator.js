64015de6e8f600930a1a42bc5de806016e2ac0ea
Saksury098 <165182717+Saksury098@users.noreply.github.com>
2024-07-27 17:51:47 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,20 @@
+let string = "";
+// query selector jiski bhi class button hai unko select karega
+let buttons = document.querySelectorAll('.button'); 
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == '=' ){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+       else if(e.target.innerHTML == 'C' ){
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else{
+        console.log(e.target)
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+        }
+    })
+})
